304db7d62db1fe83d9afb71a068d69a8dbfbca18
joaopaulu <jptick@gmail.com>
2023-06-09 15:08:37 -0300

    Implementando o endpoint de cadastro do recurso de autuação

--- src/main/java/com/algaworks/algatransito/domain/service/RegistroVeiculoService.java
+++ src/main/java/com/algaworks/algatransito/domain/service/RegistroVeiculoService.java
@@ -1,5 +1,6 @@
 package com.algaworks.algatransito.domain.service;
 
+import com.algaworks.algatransito.domain.exception.EntidadeNaoEncontradaException;
 import com.algaworks.algatransito.domain.exception.NegocioException;
 import com.algaworks.algatransito.domain.model.Proprietario;
 import com.algaworks.algatransito.domain.model.StatusVeiculo;
@@ -17,6 +18,11 @@ public class RegistroVeiculoService {
 
     private final VeiculoRepository veiculoRepository;
     private final RegistroProprietarioService registroProprietarioService;
+
+    public Veiculo buscar(Long veiculoId) {
+        return veiculoRepository.findById(veiculoId)
+                .orElseThrow(() -> new EntidadeNaoEncontradaException("Veículo não encontrado"));
+    }
 
     @Transactional
     public Veiculo cadastrar(Veiculo novoVeiculo) {
